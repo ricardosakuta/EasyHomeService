4f5e395014ec17dafea92a6ce7024a95da2bb62a
ricardosakuta <ricardo.toshio.s@gmail.com>
2021-03-15 22:17:57 -0400

    Modelo básico de comunicação com API

--- client/src/Pages/Buscar.js
+++ client/src/Pages/Buscar.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Buscar() {
-    return (
-        <h1>Buscar</h1>
-    )
+class Buscar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: ''
+        };
+      }
+    
+      componentDidMount() {
+        fetch("http://localhost:3000/api/buscar")
+          .then(res => res.json())
+          .then(
+            (result) => {
+                
+              this.setState({
+                name: result.name
+              });
+            },
+          )
+      }
+
+    render () {
+        const { name } = this.state;
+
+        return (
+            <h1>{name}</h1>
+        )
+    }
 }
+
+export default Buscar
